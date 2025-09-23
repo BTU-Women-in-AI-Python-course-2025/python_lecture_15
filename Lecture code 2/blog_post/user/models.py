06088bd5ad49e3970c92444c2fe03ecdc29b98c1
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-23 19:58:36 +0400

    Remove User import

--- Lecture code 2/blog_post/user/models.py
+++ Lecture code 2/blog_post/user/models.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, User
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 from django.db import models
-
-User
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
