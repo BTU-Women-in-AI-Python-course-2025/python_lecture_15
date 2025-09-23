cdde0c75925b9818ebfd2f99e1ca126a9c045921
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-23 20:10:54 +0400

    Create UserRegisterView

--- Lecture code 2/blog_post/user/views.py
+++ Lecture code 2/blog_post/user/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from user.forms import UserRegistrationForm
+from user.models import CustomUser
 
-# Create your views here.
+class UserRegisterView(CreateView):
+    model = CustomUser
+    form_class = UserRegistrationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('class_blog_list')
