bac20f0772a8a33f6dabf11fa5c214e6454dc0b4
MariamKipshidze <mari.kifshidze@gmail.com>
2025-09-23 19:57:54 +0400

    Create CustomUser

--- Lecture code 2/blog_post/blog_post/settings.py
+++ Lecture code 2/blog_post/blog_post/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'blog'
+    'blog',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.CustomUser'
